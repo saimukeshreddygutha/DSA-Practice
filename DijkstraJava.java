915a4408e9d427c73e977801687da17136f84c9a
Gutha Sai Mukesh Reddy <78792429+saimukeshreddygutha@users.noreply.github.com>
2022-03-13 17:11:34 +0530

    Update DijkstraJava.java

--- DijkstraJava.java
+++ DijkstraJava.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class DijkstraJava {
 
 	public static int selectU(boolean visited[], int distance[], int n){
-
+		//finding a node that is unvisited and is minimum distance from source_node
 		int min = Integer.MAX_VALUE;
 		int v=0;
 
@@ -35,13 +35,21 @@ public class DijkstraJava {
 
 
 		for(int i=1;i<n;i++){
-
+			//select a node that is not visited and adjacent to the previously relaxed node
 			int u = selectU(visited,distance,n);
 
 
 			visited[u]=true;
 
 			for(int j=0;j<n;j++){
+				/*
+				Conditions to relax a node and update their distances
+				1. the node must be visited already
+				2. manage distance overflow distance[u]!=Integer.MAX_VALUE
+				3. there should be an edge in the graph
+				4. distance through u must be a benifit for us
+				
+				*/
 
 				if((visited[j]==false) && (distance[u]!=Integer.MAX_VALUE) && (cost[u][j]!=0) && (distance[j]>distance[u]+cost[u][j])){
 					distance[j]=distance[u]+cost[u][j];
@@ -67,6 +75,10 @@ public class DijkstraJava {
 	}
 
 	public static void main(String[] args) {
+		
+		/*
+		Use your own way of input and output
+		*/
 
 		try {
 			System.setIn(new FileInputStream("input.txt"));
