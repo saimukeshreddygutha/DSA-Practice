e63cb70d39801993c89e8c9223382cffbb73a310
Gutha Sai Mukesh Reddy <78792429+saimukeshreddygutha@users.noreply.github.com>
2022-03-16 11:14:09 +0530

    Updated countStrings with O(n3) TC
    Count Substrings That Differ by One Character
    https://leetcode.com/problems/count-substrings-that-differ-by-one-character/

--- countSubstrings.java
+++ countSubstrings.java
@@ -4,23 +4,20 @@ class Solution {
         int slen = s.length();
         int tlen = t.length();
         int ans =0;
-        for(int len = 1;len <= slen;len++){
-            
-            for(int sp = 0; sp <= slen-len; sp++){
-                
-                for(int tp=0; tp <=tlen-len;tp++){
-                    int diff=0;
-                    int i = 0;
-                    while(i<len){
-                        if(s.charAt(i+sp)!=t.charAt(i+tp))
-                            diff+=1;
-                        if(diff > 1)
-                            break;
-                        i++;
-                    }
+        for(int sp=0;sp<slen;sp++){
+            for(int tp=0;tp<tlen;tp++){
+                int diff=0;
+                int x=sp;
+                int y=tp;
+                while(x<slen && y<tlen){
+                    if(s.charAt(x)!=t.charAt(y))
+                        diff++;
                     if(diff==1)
-                        ans+=1;
-                    
+                        ans++;
+                    if(diff>1)
+                        break;
+                    x++;
+                    y++;
                 }
             }
         }
