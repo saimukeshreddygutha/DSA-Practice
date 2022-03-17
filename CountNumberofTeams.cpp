e24276e06f7667e808f97d3c52a70e8b8151570b
Gutha Sai Mukesh Reddy <78792429+saimukeshreddygutha@users.noreply.github.com>
2022-03-18 01:57:00 +0530

    Create CountNumberofTeams.cpp
    Count Number of Teams
    https://leetcode.com/problems/count-number-of-teams/

---
+++ CountNumberofTeams.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int numTeams(vector<int>& rating) {
+        
+        int n = rating.size();
+        int ans = 0;
+        
+        for(int i = 0;i < n-2;i++){
+            for(int j = i+1 ; j < n-1;j++){
+                if(rating[i]<rating[j]){
+                    for(int k = j+1;k < n;k++){
+                        if(rating[j]<rating[k])
+                            ans++;
+                    }
+                }
+                else{
+                    for(int k = j+1;k < n;k++){
+                        if(rating[j]>rating[k])
+                            ans++;
+                    }
+                }
+            }
+        }
+        
+        return ans;
+    }
+};
