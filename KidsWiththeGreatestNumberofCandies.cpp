aef731e19fb63727af33799657a2be808db2a9d3
Gutha Sai Mukesh Reddy <78792429+saimukeshreddygutha@users.noreply.github.com>
2022-04-10 13:38:48 +0530

    Create KidsWiththeGreatestNumberofCandies.cpp
    Kids With the Greatest Number of Candies
    https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/

---
+++ KidsWiththeGreatestNumberofCandies.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        
+        vector<bool> ans;
+        
+        int mx = *max_element(candies.begin(), candies.end());
+        
+        for(auto x: candies){
+            if(x + extraCandies >= mx)
+                ans.push_back(true);
+            else
+                ans.push_back(false);
+        }
+        
+        return ans;
+        
+    }
+};
