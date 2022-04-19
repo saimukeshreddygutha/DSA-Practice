1f14fb28b718f0b34a3dc9c6d05f98ec59ffcd7e
Gutha Sai Mukesh Reddy <78792429+saimukeshreddygutha@users.noreply.github.com>
2022-04-19 09:14:15 +0530

    Create PeakIndexinaMountainArray.cpp
    Peak Index in a Mountain Array
    https://leetcode.com/problems/peak-index-in-a-mountain-array/

---
+++ PeakIndexinaMountainArray.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        
+        int h = arr.size() - 1;
+        int l = 0;
+        
+        
+        while(l < h){
+            int m = (l + h)/2;
+            if(arr[m] < arr[m + 1])
+                l = m + 1;
+            else
+                h = m;
+        }
+        
+        return l;
+    }
+};
