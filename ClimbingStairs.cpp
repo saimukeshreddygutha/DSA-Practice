daa7613a321569105474c18397ecf1f7b585e4e0
Gutha Sai Mukesh Reddy <78792429+saimukeshreddygutha@users.noreply.github.com>
2022-04-27 07:29:19 +0530

    Create ClimbingStairs.cpp
    Climbing Stairs
    https://leetcode.com/problems/climbing-stairs/

---
+++ ClimbingStairs.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        int a[46]={0};
+        a[1] = 1;
+        a[2] = 2;
+        for(int i = 3;i < 46;i++)
+            a[i] = a[i - 1] + a[i - 2];
+        
+        return a[n];
+    }
+};
