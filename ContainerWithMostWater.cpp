3b8045a3310896f156c914c107b67999248afa7b
Gutha Sai Mukesh Reddy <78792429+saimukeshreddygutha@users.noreply.github.com>
2022-05-05 09:55:20 +0530

    Create ContainerWithMostWater.cpp
    Container With Most Water (2 pointer Approach)
    https://leetcode.com/problems/container-with-most-water/

---
+++ ContainerWithMostWater.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        
+        int i = 0, j = height.size()-1;
+        int ans = INT_MIN;
+        while(i < j){
+            ans = max(ans, min(height[i], height[j])*(j-i));
+            if(height[i] < height[j])i++;
+            else j--;
+        }
+        
+        return ans;
+    }
+};
