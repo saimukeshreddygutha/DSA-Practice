bca9c275d6b33b364a2792ab73d58ae47e9d0703
Gutha Sai Mukesh Reddy <78792429+saimukeshreddygutha@users.noreply.github.com>
2022-05-20 22:33:02 +0530

    Create FirstMissingPositive.cpp
     First Missing Positive
    https://leetcode.com/problems/first-missing-positive/

---
+++ FirstMissingPositive.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums){
+        sort(nums.begin(), nums.end());
+		int count = 1;
+            
+		for(int i = 0; i < nums.size(); i++){
+		   if(nums[i]==count)
+                count++;
+            else if(nums[i]>count)
+                return count;
+        }
+		return count;
+    }
+};
