28a09900f2ba7f82b2b4fcfdf8ab87f96ed7f826
Gutha Sai Mukesh Reddy <78792429+saimukeshreddygutha@users.noreply.github.com>
2022-06-09 22:19:50 +0530

    Create CountPrimes.cpp
    Count Primes
    https://leetcode.com/problems/count-primes/

---
+++ CountPrimes.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        if(n < 2)return 0;
+        bool prime[n];
+        fill(prime, prime + n, true);
+        prime[0] = prime[1] = false;
+        for(int i = 2;i * i < n;i++){
+            if(prime[i]){
+                for(int j = i * i;j < n;j += i)prime[j] = false;
+            }
+        }
+        int count = 0;
+        for(int i = 2;i < n ;i++)
+            if(prime[i])count++;
+        return count;
+    }
+};
