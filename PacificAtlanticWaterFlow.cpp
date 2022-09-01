2ca2a74ca2a9469ae2b4e78e554655949875d98d
Gutha Sai Mukesh Reddy <78792429+saimukeshreddygutha@users.noreply.github.com>
2022-09-01 23:19:12 +0530

    Update PacificAtlanticWaterFlow.cpp
    Pacific Atlantic Water Flow
    https://leetcode.com/problems/pacific-atlantic-water-flow/

--- PacificAtlanticWaterFlow.cpp
+++ PacificAtlanticWaterFlow.cpp
@@ -1,56 +1,67 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>&heights,vector<vector<int>>&oceans,int i,int j,int n,int m)
-    {
-        if(i<0 || i>n-1 || j<0 || j>m-1) return;
-        if(!oceans[i][j])
-        {
-            oceans[i][j]=1;
-            if(i-1>=0 && heights[i-1][j]>=heights[i][j]) dfs(heights,oceans,i-1,j,n,m);
-            if(i+1<n && heights[i+1][j]>=heights[i][j]) dfs(heights,oceans,i+1,j,n,m); 
-            if(j-1>=0 && heights[i][j-1]>=heights[i][j]) dfs(heights,oceans,i,j-1,n,m);
-            if(j+1<m && heights[i][j+1]>=heights[i][j]) dfs(heights,oceans,i,j+1,n,m);
-        }
+    
+    
+    void dfs(vector<vector<int>> &h, vector<vector<int>> &v, int i, int j, int m, int n){
+        
+        if(i < 0 || j < 0 || i >= m || j >= n)return;
+        
+        
+        
+        v[i][j] = 1;
+        if(i > 0 && h[i - 1][j] >= h[i][j] && !v[i - 1][j])dfs(h, v, i - 1, j, m, n);
+        if(j > 0 && h[i][j - 1] >= h[i][j] && !v[i][j - 1])dfs(h, v, i, j - 1, m, n);
+        if(i < m - 1 && h[i + 1][j] >= h[i][j] && !v[i + 1][j])dfs(h, v, i + 1, j, m, n);
+        if(j < n - 1 && h[i][j + 1] >= h[i][j] && !v[i][j + 1])dfs(h, v, i, j + 1, m, n);
+        
     }
-    vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
-        int n=heights.size();
-        int m=heights[0].size();
-        vector<vector<int>>pacific(n,vector<int>(m,0));
-        vector<vector<int>>atlantic(n,vector<int>(m,0));
-        vector<vector<int>>ans;
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
-                if(i==0 || j==0){
-                    if(!pacific[i][j])
-                    {
-                        dfs(heights,pacific,i,j,n,m);
-                    }
-                }
+    
+    vector<vector<int>> pacificAtlantic(vector<vector<int>>& h) {
+        
+        int r = h.size();
+        int c = h[0].size();
+        
+        vector<vector<int>> ans;
+        
+        vector<vector<int>> p(r, vector<int>(c, 0));
+        vector<vector<int>> a(r, vector<int>(c, 0));
+        
+        
+        for(int i = 0;i < r;i++){
+            if(p[i][0] != 1){
+                p[i][0] = 1;
+                dfs(h, p, i, 0, r, c);
+            }
+        }
+        for(int i = 1;i < c;i++){
+            if(p[0][i] != 1){
+                p[0][i] = 1;
+                dfs(h, p, 0, i, r, c);
             }
         }
         
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
-                if(i==n-1 || j==m-1){
-                    if(!atlantic[i][j])
-                    {
-                        dfs(heights,atlantic,i,j,n,m);
-                    }
-                }
+        for(int i = 0;i < r;i++){
+            if(a[i][c - 1] != 1){
+                a[i][c - 1] = 1;
+                dfs(h, a, i, c - 1, r, c);
             }
         }
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
-                if(pacific[i][j] && atlantic[i][j])
-                    ans.push_back({i,j});
+        for(int i = 0;i < c - 1;i++){
+            if(a[r - 1][i] != 1){
+                a[r - 1][i] = 1;
+                dfs(h, a, r - 1, i, r, c);
             }
         }
+                 
+        for(int i = 0;i < r;i++){
+            for(int j = 0;j< c;j++){
+                if(p[i][j] && a[i][j])ans.push_back({i, j});
+                
+            }
+            
+        }
+        
+                 
         return ans;
     }
 };
