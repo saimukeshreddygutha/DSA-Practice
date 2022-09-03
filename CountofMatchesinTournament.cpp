3b1818ef49ed90f12103b7e77db8186620cdd2b0
Gutha Sai Mukesh Reddy <78792429+saimukeshreddygutha@users.noreply.github.com>
2022-09-03 23:42:32 +0530

    Create CountofMatchesinTournament.cpp
    Count of Matches in Tournament
    https://leetcode.com/problems/count-of-matches-in-tournament/

---
+++ CountofMatchesinTournament.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int numberOfMatches(int n) {
+        int ans = 0;
+        while(n != 1){
+            ans += n/2;
+            if((n&1)){
+                n = (n/2) + 1;
+            }
+            else{
+                
+                n /= 2;
+            }
+            
+        }
+        return ans;
+    }
+};
