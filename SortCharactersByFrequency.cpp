721d9de7937ba5fad607251dc6cc360917ec68aa
Gutha Sai Mukesh Reddy <78792429+saimukeshreddygutha@users.noreply.github.com>
2022-09-09 23:28:00 +0530

    Update SortCharactersByFrequency.cpp

--- SortCharactersByFrequency.cpp
+++ SortCharactersByFrequency.cpp
@@ -1,26 +1,28 @@
-class Solution
-{
+class Solution {
 public:
-    string frequencySort(string s)
-    {
-        map<char, int> mp;
-        for (int i = 0; i < s.length(); i++)
-            ++mp[s[i]];
-
+    string frequencySort(string s) {
+        unordered_map<char, int> m;
+        int l = s.size();
+        for(int i = 0;i < l;i++){
+            m[s[i]]++;
+        }
         priority_queue<pair<int, char>> pq;
-        for (auto i : mp)
-        {
-            pq.push({i.second, i.first});
+        unordered_map<char, int> :: iterator itr;
+        for(itr = m.begin();itr != m.end();itr++){
+            pq.push(make_pair(itr -> second, itr -> first));
         }
+        
         string ans = "";
-        while (!pq.empty())
-        {
-            for (int i = 0; i < pq.top().first; i++)
-            {
-                ans += pq.top().second;
-            }
+        while(!pq.empty()){
+            pair<int, char> t = pq.top();
             pq.pop();
+            int n = t.first;
+            char c = t.second;
+            while(n--)ans += c;
         }
+        
         return ans;
+        
+        
     }
 };
