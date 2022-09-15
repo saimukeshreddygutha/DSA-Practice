bcb99930429115826b281acaa58e8909ffa45db1
Gutha Sai Mukesh Reddy <78792429+saimukeshreddygutha@users.noreply.github.com>
2022-09-15 23:59:32 +0530

    Create RangeSumQuery-Immutable.cpp
    Range Sum Query - Immutable
    https://leetcode.com/problems/range-sum-query-immutable/

---
+++ RangeSumQuery-Immutable.cpp
@@ -0,0 +1,14 @@
+class NumArray {
+public:
+    vector<int> accu;
+    NumArray(vector<int> &nums) {
+        accu.push_back(0);
+        for (int num : nums)
+            accu.push_back(accu.back() + num);
+    }
+
+    int sumRange(int i, int j) {
+        return accu[j + 1] - accu[i];
+    }
+    
+};
