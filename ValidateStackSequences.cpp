c7cff416ea2c9d92077ec1cab565f9d00a12d2e8
Gutha Sai Mukesh Reddy <78792429+saimukeshreddygutha@users.noreply.github.com>
2022-09-20 00:00:49 +0530

    Update ValidateStackSequences.cpp

--- ValidateStackSequences.cpp
+++ ValidateStackSequences.cpp
@@ -5,7 +5,7 @@ public:
         int idx = 0;
         for(int i = 0;i < popped.size();i++){
             s.push(pushed[i]);
-            while(idx < popped.size() && s.top() == popped[idx]){s.pop();idx++;}
+            while(!s.empty() && idx < popped.size() && s.top() == popped[idx]){s.pop();idx++;}
         }
         
         return idx == popped.size();
