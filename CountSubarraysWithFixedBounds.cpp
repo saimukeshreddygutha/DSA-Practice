08be0cdf3e8b02d2967244b18adcf457a157c9ea
Gutha Sai Mukesh Reddy <78792429+saimukeshreddygutha@users.noreply.github.com>
2022-10-18 00:03:41 +0530

    Update CountSubarraysWithFixedBounds.cpp
    Count Subarrays With Fixed Bounds
    https://leetcode.com/problems/count-subarrays-with-fixed-bounds/

--- CountSubarraysWithFixedBounds.cpp
+++ CountSubarraysWithFixedBounds.cpp
@@ -9,7 +9,6 @@ public:
         int mx = -1;
         int start = 0;
         for(int i = 0;i < n;i++){
-            bool changed = false;
             if(nums[i] < minK || nums[i] > maxK){start = i + 1;mn = -1;mx = -1;}
             if(nums[i] == minK){mn = i;}
             if(nums[i] == maxK){mx = i;}
