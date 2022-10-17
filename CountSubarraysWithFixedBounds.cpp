e63c88af112207c54e17aa9618b2d676a1aa16d6
Gutha Sai Mukesh Reddy <78792429+saimukeshreddygutha@users.noreply.github.com>
2022-10-18 00:02:12 +0530

    Create CountSubarraysWithFixedBounds.cpp
    Count Subarrays With Fixed Bounds
    https://leetcode.com/problems/count-subarrays-with-fixed-bounds/

---
+++ CountSubarraysWithFixedBounds.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    long long countSubarrays(vector<int>& nums, int minK, int maxK) {
+        
+        
+        long long ans = 0;
+        int n = nums.size();
+        int mn = -1;
+        int mx = -1;
+        int start = 0;
+        for(int i = 0;i < n;i++){
+            bool changed = false;
+            if(nums[i] < minK || nums[i] > maxK){start = i + 1;mn = -1;mx = -1;}
+            if(nums[i] == minK){mn = i;}
+            if(nums[i] == maxK){mx = i;}
+            if(mn != -1 && mx != -1)ans += (min(mn, mx) - start + 1); 
+            
+        }
+        
+        return ans;
+    }
+};
