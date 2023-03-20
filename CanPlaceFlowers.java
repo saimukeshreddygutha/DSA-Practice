483fd19ec0266dd65983a548ceb850b346a2cc8e
Gutha Sai Mukesh Reddy <78792429+saimukeshreddygutha@users.noreply.github.com>
2023-03-20 14:37:16 +0530

    Create CanPlaceFlowers.java
    Can Place Flowers
    https://leetcode.com/problems/can-place-flowers/description/

---
+++ CanPlaceFlowers.java
@@ -0,0 +1,10 @@
+class Solution {
+
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+    for (int i = 0; i < flowerbed.length && n > 0; i ++)
+        if (flowerbed [i] == 0 && (i == 0 || flowerbed [i - 1] == 0) && (i == flowerbed.length - 1 || flowerbed [i + 1] == 0)) {
+            n--;
+            flowerbed [i] = 1;}
+    return n == 0;
+}
+}
