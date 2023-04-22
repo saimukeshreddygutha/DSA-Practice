759ba465041523c9a9e15a1b3fc1600410556548
Gutha Sai Mukesh Reddy <78792429+saimukeshreddygutha@users.noreply.github.com>
2023-04-22 13:14:58 +0530

    Create MinimumInsertionStepstoMakeaStringPalindrome.java
    Minimum Insertion Steps to Make a String Palindrome
    https://leetcode.com/problems/minimum-insertion-steps-to-make-a-string-palindrome/description/

---
+++ MinimumInsertionStepstoMakeaStringPalindrome.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int longestCommonSubsequence(String s, String r){
+
+        int n = s.length();
+        int[][] dp = new int[n+1][n+1];
+
+
+        for(int i = 1;i < n + 1;i++){
+            for(int j = 1;j < n + 1;j++){
+                if(s.charAt(i-1) == r.charAt(j-1))dp[i][j] = dp[i-1][j-1] + 1;
+                else dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+
+        return dp[n][n];
+    }
+    public int minInsertions(String s) {
+        StringBuilder r = new StringBuilder("");
+        for(int i = s.length() - 1;i > -1;i--){
+            r.append(s.charAt(i));
+        }
+        return s.length() - longestCommonSubsequence(s, r.toString());
+    }
+}
